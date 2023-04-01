7cb4669e42b2daea8bfe25ab462679482fcfeafc
Dave Pearson <davep@davep.org>
2023-04-01 08:51:35 +0100

    :lipstick: Try and make best use of the display on startup

--- textual_mandelbrot/__main__.py
+++ textual_mandelbrot/__main__.py
@@ -1,6 +1,10 @@
 """Mandelbrot plotting application for the terminal."""
 
 ##############################################################################
+# Python imports.
+from __future__ import annotations
+
+##############################################################################
 # Textual imports.
 from textual.app     import App, ComposeResult
 from textual.widgets import Header, Footer
@@ -24,15 +28,54 @@ class MandelbrotApp( App[ None ] ):
 
     Mandelbrot {
         border: heavy grey;
-        width: 130;
-        height: 50;
     }
     """
+
+    def _best_size( self ) -> tuple[ tuple[ int, int ], tuple[ int, int ] ]:
+        """Figure out the best initial size for the plot and the widget.
+
+        Returns:
+            A `tuple` of `tuple`s if two `int`. The first `tuple` is the
+            suggested width and height of the canvas itself. The second is
+            the suggested width and height for the widget.
+        """
+
+        # Get the initial width/height of the display. Note the little bit
+        # of "magic number" work here; 2 is allowing for the border of the
+        # Canvas widget; 4 is allowing for the border of the Canvas widget
+        # and the Header and Footer widgets.
+        display_width = self.app.console.width - 2
+        display_height = self.app.console.height - 4
+
+        # Go for 4:3 based off the width.
+        best_width  = display_width
+        best_height = ( ( display_width // 4 ) * 3 ) // 2
+
+        # If that looks like it isn't going to fit nicely...
+        if best_height >= display_height:
+            # ...let's try and make it fit from the height first.
+            best_height = display_height
+            best_width  = ( ( best_height // 3 ) * 4 ) * 2
+
+        # Final choice.
+        return ( best_width - 2, ( best_height - 2 ) * 2 ), ( best_width, best_height )
+
+    def _mandelbrot( self ) -> Mandelbrot:
+        """Create the Mandelbrot plotting widget.
+
+        Returns:
+            The widget.
+        """
+        ( canvas_width, canvas_height ), ( widget_width, widget_height ) = self._best_size()
+        plot = Mandelbrot( canvas_width, canvas_height )
+        plot.styles.width  = widget_width
+        plot.styles.height = widget_height
+        return plot
 
     def compose( self ) -> ComposeResult:
         """Compose the child widgets."""
         yield Header()
-        yield Mandelbrot( 128, 96 )
+        yield self._mandelbrot()
         yield Footer()
 
     def on_mount( self ) -> None:
@@ -46,11 +89,9 @@ class MandelbrotApp( App[ None ] ):
             event: The event with the change details.
         """
         self.sub_title = (
-            f"{event.from_x:.2f}, "
-            f"{event.from_y:.2f} -> "
-            f"{event.to_x:.2f}, "
-            f"{event.to_y:.2f} -- "
-            f"{event.max_iteration} iterations"
+            f"( {event.from_x:.2f}, {event.from_y:.2f} ) -> "
+            f"( {event.to_x:.2f}, {event.to_y:.2f} )"
+            f" | {event.max_iteration} iterations"
         )
 
 ##############################################################################
