c4e2f02f66bd619a265a0562a5f60631a1864bbf
Dave Pearson <davep@davep.org>
2023-04-02 08:37:44 +0100

    :books: Add missing arguments from _mandelbrot

--- textual_mandelbrot/mandelbrot.py
+++ textual_mandelbrot/mandelbrot.py
@@ -37,13 +37,19 @@ def colour_map( value: int, max_iteration: int ) -> Color:
 def _mandelbrot( x: Decimal, y: Decimal, multibrot: float, max_iteration: int ) -> int:
     """Return the Mandelbrot calculation for the point.
 
+    Args:
+        x: The x location of the point to calculate.
+        y: The y location of the point to calculate.
+        multibrot: The 'multibrot' value to use in the calculation.
+        max_iteration: The maximum number of iterations to calculate for.
+
     Returns:
         The number of loops to escape, or 0 if it didn't.
 
     Note:
         The point is considered to be stable, considered to have not
-        escaped, if the ``max_iteration`` has been hit without the
-        calculation going above 2.0.
+        escaped, if the `max_iteration` has been hit without the calculation
+        going above 2.0.
     """
     c1 = complex( x, y )
     c2 = 0j
